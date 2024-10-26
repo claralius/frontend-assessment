e61d448483b976f1e513171eddec4fcc429703cf
Clarabella Lius <liusclarabella@gmail.com>
2024-10-26 16:00:08 +0700

    fix: last changes

--- juicebox-frontend-assessment/src/app/homepage/page.tsx
+++ juicebox-frontend-assessment/src/app/homepage/page.tsx
@@ -6,35 +6,38 @@ import Image from "next/image";
 import RoundedHexagon from "../../assets/icons/rounded-hexagon.png";
 import Navbar from "../components/navbar/Navbar";
 import ContinueButton from "../components/buttons/ContinueButton";
+import {ReactLenis} from '@/utils/lenis'
 
 const PublicHomepage = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <section className="mid-section">
-        <div className="mid-text-left">
-          <p className="caption-text">WA businesses feel confident about future growth </p>
-          <p className="caption-text">Sales measure true success</p>
-          <p className="caption-text">The primary barrier to digital transformation is financial investment</p>
-        </div>
-        <Image className="hexagon-image" src={RoundedHexagon} alt="rounded-hexagon" />
-        <div className="mid-text-right">
-          <p className="caption-text">AI cant replace creativity</p>
-          <p className="caption-text">Human connection drives WA business</p>
-        </div>
-      </section>
-      <section className="text-section">
-        <h2>Compare your thoughts on  
-          <span> technology </span> 
-          with current industry opinions.</h2>
-      </section>
+      <ReactLenis> 
+        <section className="mid-section">
+          <div className="mid-text-left">
+            <p className="caption-text">WA businesses feel confident about future growth </p>
+            <p className="caption-text">Sales measure true success</p>
+            <p className="caption-text">The primary barrier to digital transformation is financial investment</p>
+          </div>
+          <Image className="hexagon-image" src={RoundedHexagon} alt="rounded-hexagon" />
+          <div className="mid-text-right">
+            <p className="caption-text">AI cant replace creativity</p>
+            <p className="caption-text">Human connection drives WA business</p>
+          </div>
+        </section>
+        <section className="text-section">
+          <h2>Compare your thoughts on  
+            <span> technology </span> 
+            with current industry opinions.</h2>
+        </section>
 
-      <section className="bottom-section">
-       <ContinueButton placeholder={"Get a reality check"} className="reality-check-btn"></ContinueButton>  
-      </section>
-      {/* <Link href="/tutorial" style={{ color: "white" }}>
-        rEDIRECT TO TUTORIALS
-      </Link> */}
+        <section className="bottom-section">
+        <ContinueButton placeholder={"Get a reality check"} className="reality-check-btn"></ContinueButton>  
+        </section>
+        {/* <Link href="/tutorial" style={{ color: "white" }}>
+          rEDIRECT TO TUTORIALS
+        </Link> */}
+      </ReactLenis>
     </div>
   );
 };
