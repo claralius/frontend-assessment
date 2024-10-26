b8bc88680cbffc3f8a836a9374086660cc3fe823
Clarabella Lius <liusclarabella@gmail.com>
2024-10-27 00:54:39 +0700

    feat: succesful use continue button to swipe next slide

--- juicebox-frontend-assessment/src/app/components/tutorial/SwiperComponent.tsx
+++ juicebox-frontend-assessment/src/app/components/tutorial/SwiperComponent.tsx
@@ -1,40 +1,76 @@
 "use client";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import type SwiperCore from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-//modules
 import { Pagination, Navigation } from "swiper/modules";
 import "./SwiperComponent.scss";
+import "../../../styles/modules/typography.scss";
+import Image from "next/image";
+import RoundedHexagon from "../../../assets/icons/rounded-hexagon.png";
+import { useEffect, useImperativeHandle, useRef } from "react";
+import React from "react";
 
-const SwiperComponent = () => {
+
+const SwiperComponent = React.forwardRef((props, ref) => {
+  const swiperRef = useRef<SwiperCore | null>(null);
+
+  // Expose swiperRef methods to parent component
+  useImperativeHandle(ref, () => swiperRef.current);
   return (
     <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
+      onSwiper={(swiper) => (swiperRef.current = swiper)}
+      spaceBetween={10}
+      slidesPerView={1}
       pagination={{ clickable: true }}
-      scrollbar= {{ draggable:true }}
-      className='swiper-container'
-      loop = {true}
-      modules= {[Pagination, Navigation]}
+      navigation={false}
+      scrollbar={{ draggable: true }}
+      className="swiper-container"
+      loop={false}
+      modules={[Pagination, Navigation]}
     >
-        <SwiperSlide>
-            <h1>jalan</h1>          
-        </SwiperSlide>
-        <SwiperSlide>
-            <h1>jalan2</h1>
-        </SwiperSlide>
-        <SwiperSlide>
-            <h1>jalan3</h1>
-        </SwiperSlide>
-        <SwiperSlide>
-            <h1>jalan4</h1>
-        </SwiperSlide>
-        <SwiperSlide>
-            <h1>jalan5</h1>
-        </SwiperSlide>
+      <SwiperSlide className="first-slide">
+        <Image
+          className="hexagon-image"
+          src={RoundedHexagon}
+          alt="rounded-hexagon"
+        />
+        <h4 className="tutorial-text">
+          <span className="tutorial-text-span">
+            Professionals around the world shared how they feel abo
+          </span>
+          ut technology and I’ve listened. Now it’s your turn.
+        </h4>
+      </SwiperSlide>
+      <SwiperSlide className="second-slide">
+        <Image
+          className="hexagon-image"
+          src={RoundedHexagon}
+          alt="rounded-hexagon"
+        />
+        <h4 className="tutorial-text">
+          <span className="tutorial-text-span">
+            I’ll ask you a handful of meaningful questions
+          </span>
+          &nbsp; and compare your responses with people in your industry. 
+        </h4>
+      </SwiperSlide>
+      <SwiperSlide className="third-slide">
+        <Image
+          className="hexagon-image"
+          src={RoundedHexagon}
+          alt="rounded-hexagon"
+        />
+        <h4 className="tutorial-text">
+          <span className="tutorial-text-span">
+            You’ll get insights into current industry sentiments an
+          </span>
+          d a reality check about technology in a few minutes. Deal? Great!
+        </h4>
+      </SwiperSlide>
     </Swiper>
   );
-};
+});
 
 export default SwiperComponent;
--- juicebox-frontend-assessment/src/app/tutorials/page.tsx
+++ juicebox-frontend-assessment/src/app/tutorials/page.tsx
@@ -1,11 +1,43 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import SwiperComponent from "../components/tutorial/SwiperComponent";
+import Navbar from "../components/navbar/Navbar";
+import "./tutorial.scss";
+import Link from "next/link";
+import ContinueButton from "../components/buttons/ContinueButton";
+import { useRouter } from 'next/navigation'
+import Swiper from "swiper";
 
 const TutorialPage = () => {
+  const swiperRef = useRef<Swiper | null>(null); // Ref for the Swiper instance
+  const router = useRouter(); // Get router instance
+  const handleNextSlide = () => {
+    if (swiperRef.current) {
+      const currentIndex = swiperRef.current.activeIndex; // Get current slide index
+      const totalSlides = swiperRef.current.slides ? swiperRef.current.slides.length : 0; // Get total number of slides
+      
+      if (currentIndex < totalSlides - 1) {
+        swiperRef.current.slideNext(); // Navigate to the next slide
+      } else {
+        router.push("/tutorials"); // Navigate to tutorials page if it's the last slide
+      }
+    }
+  };
   return (
     <>
-      <h1 style={{ color:'white' }}>asas</h1>
-      <SwiperComponent/>
+      <Navbar></Navbar>
+      <section className="tutorial-mid-section">
+        <SwiperComponent ref={swiperRef} />
+      </section>
+      <section className="bottom-section">
+        <Link href="/tutorials">
+          <ContinueButton
+            placeholder={"Continue"}
+            className="continue-btn"
+            onClick={handleNextSlide}
+          ></ContinueButton>
+        </Link>
+      </section>
     </>
   );
 };
