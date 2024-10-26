a59a0df6fa2e833470fb3122e15c080b76e88850
Clarabella Lius <liusclarabella@gmail.com>
2024-10-26 18:33:00 +0700

    init: init structure folder tutorial

---
+++ juicebox-frontend-assessment/src/app/tutorials/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const TutorialPage = () => {
+  return (
+    <div>
+      <h1>tutorisl</h1>
+    </div>
+  )
+}
+
+export default TutorialPage
