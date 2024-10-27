bddc9b62271de6b671e0eaed8d328237304aea67
Clarabella Lius <liusclarabella@gmail.com>
2024-10-27 11:49:50 +0700

    feat: finish input form name

--- juicebox-frontend-assessment/src/app/multistep-form/page.tsx
+++ juicebox-frontend-assessment/src/app/multistep-form/page.tsx
@@ -1,33 +1,62 @@
 "use client";
-import React from 'react';
-import { useEffect, useRef } from 'react';
+import React, { useState } from "react";
 import "./form.scss";
-import Navbar from '../components/navbar/Navbar';
-import Lottie from 'lottie-react';
+import Navbar from "../components/navbar/Navbar";
+import Lottie from "lottie-react";
 import JB2G from "../../assets/lottie/JB2G_Lottie.json";
 
-const MultiStepFormPage = () => {
-  // const hexagonAnimation = useRef(null);
+const SendButtonSVG = () => (
+  <svg width="31" height="32" viewBox="0 0 31 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g opacity="0.5">
+      <rect y="0.5" width="31" height="31" rx="15.5" fill="currentColor" fillOpacity="0.6" />
+      <path d="M10.75 14.5L16 9.25M16 9.25L21.25 14.5M16 9.25V22.75" stroke="currentColor" />
+    </g>
+  </svg>
+);
 
-  // useEffect(() => {
-  //   lottie.loadAnimation({
-  //     container: hexagonAnimation.current,
-  //     renderer: 'svg',
-  //     loop: true,
-  //     autoplay: true,
-  //     path: '/animation.json'
-  //   });
-  // }, []);
+const MultiStepFormPage = () => {
+  const [name, setName] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    const nameRegex = /^[A-Za-z]+$/;
+
+    setName(value);
+
+    if (nameRegex.test(value) || value === "") {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  };
 
   return (
-    <>
+    <div className="formpage-layout">
       <Navbar></Navbar>
-      <section className='question-section'>
-        <Lottie animationData={JB2G}/>
+      <section className="question-section">
+        <Lottie animationData={JB2G} className="lottie-animation" />
+        <h5 className="form-question">
+          Let's start with the basics. Type in your first name.
+        </h5>
       </section>
-      <h1 style={{ color:'white' }}>multi step form</h1>
-    </>
-  )
-}
+      <section className="form-section">
+        <form action="" className={`input-form ${!isValid ? 'invalid' : ''}`}>
+          <input
+            type="text"
+            name="name"
+            placeholder="First name"
+            value={name}
+            onChange={handleInputChange}
+          />
+          <div className={`send-button ${name ? 'active' : ''}`} style={{ color: name ? 'white' : 'gray' }}>
+            <SendButtonSVG />
+          </div>
+        </form>
+        {!isValid && <p className='error-message'>Invalid input. Only letters are allowed.</p>}
+      </section>
+    </div>
+  );
+};
 
-export default MultiStepFormPage
+export default MultiStepFormPage;
