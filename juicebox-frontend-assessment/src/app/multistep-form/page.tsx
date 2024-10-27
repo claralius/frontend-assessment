4b21131158e5f669562acd96cfbf581633a89697
Clarabella Lius <liusclarabella@gmail.com>
2024-10-27 09:18:52 +0700

    feat: add lottie file

--- juicebox-frontend-assessment/src/app/multistep-form/page.tsx
+++ juicebox-frontend-assessment/src/app/multistep-form/page.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { useEffect, useRef } from 'react';
 import "./form.scss";
+import Navbar from '../components/navbar/Navbar';
+import lottie from 'lottie-web';
 
 const MultiStepFormPage = () => {
+  const hexagonAnimation = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: hexagonAnimation.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: '/animation.json'
+    });
+  }, []);
+
   return (
-    <div>
+    <>
+      <Navbar></Navbar>
+      <section className='question-section'>
+        <div ref={hexagonAnimation}></div>
+      </section>
       <h1 style={{ color:'white' }}>multi step form</h1>
-    </div>
+    </>
   )
 }
 
