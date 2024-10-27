d00b3ec8d88503ceb13f95851112801015d6ff1e
Clarabella Lius <liusclarabella@gmail.com>
2024-10-27 13:13:56 +0700

    feat: successfully display name

--- juicebox-frontend-assessment/src/app/multistep-form/result/page.tsx
+++ juicebox-frontend-assessment/src/app/multistep-form/result/page.tsx
@@ -1,20 +1,18 @@
+"use client";
 import Navbar from '@/app/components/navbar/Navbar'
 import React from 'react'
 import Lottie from "lottie-react";
 import JB2G from "../../../assets/lottie/JB2G_Lottie.json";
 
-interface ResultProps{
-    name: string;
-}
-
-const ResultPage: React.FC<ResultProps> = ({name}) => {
+const ResultPage = () => {
+    const name = new URLSearchParams(window.location.search).get('name');
   return (
     <div className="formpage-layout">
       <Navbar></Navbar>
       <section className="result-text-section">
         <Lottie animationData={JB2G} className="lottie-animation" />
-        <h5 className="result-text">
-            Thanks, {name}! Now, it’s time to get a reality check. 
+        <h5 className="result-text" style={{ color:'white' }}>
+            Thanks, {name ? name : ""}! Now, it’s time to get a reality check. 
         </h5>
         <h5>
             This will take 2-3 minutes. 
