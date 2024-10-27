d33b5d3ea0f3d7cf7557ace88190383859e8ac40
Clarabella Lius <liusclarabella@gmail.com>
2024-10-27 09:24:36 +0700

    feat: add lottie file

--- juicebox-frontend-assessment/src/app/multistep-form/page.tsx
+++ juicebox-frontend-assessment/src/app/multistep-form/page.tsx
@@ -1,27 +1,29 @@
+"use client";
 import React from 'react';
 import { useEffect, useRef } from 'react';
 import "./form.scss";
 import Navbar from '../components/navbar/Navbar';
-import lottie from 'lottie-web';
+import Lottie from 'lottie-react';
+import JB2G from "../../assets/lottie/JB2G_Lottie.json";
 
 const MultiStepFormPage = () => {
-  const hexagonAnimation = useRef(null);
+  // const hexagonAnimation = useRef(null);
 
-  useEffect(() => {
-    lottie.loadAnimation({
-      container: hexagonAnimation.current,
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '/animation.json'
-    });
-  }, []);
+  // useEffect(() => {
+  //   lottie.loadAnimation({
+  //     container: hexagonAnimation.current,
+  //     renderer: 'svg',
+  //     loop: true,
+  //     autoplay: true,
+  //     path: '/animation.json'
+  //   });
+  // }, []);
 
   return (
     <>
       <Navbar></Navbar>
       <section className='question-section'>
-        <div ref={hexagonAnimation}></div>
+        <Lottie animationData={JB2G}/>
       </section>
       <h1 style={{ color:'white' }}>multi step form</h1>
     </>
